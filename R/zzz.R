b746eca4b0691b757e823f832567c4643f8e2181
Zhiao Shi <zhiao.shi@gmail.com>
2017-08-16 09:42:48 -0500

    Remove dependency on snow, use parallel instead

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,5 @@
 .onAttach <- function(lib, pkg) {
 	 packageStartupMessage("******************************************\n")
-	 packageStartupMessage("*                                        *\n")
 	 packageStartupMessage("*            Welcome to NetSAM !         *\n")
-	 packageStartupMessage("*                                        *\n")
 	 packageStartupMessage("******************************************\n")
 }
